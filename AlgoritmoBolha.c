eaacf94c1985a05d78b53f48c519f2f1848f02b1
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2022-04-30 11:13:19 -0300

    Algoritmo Bolha

--- AlgoritmoBolha.c
+++ AlgoritmoBolha.c
@@ -19,7 +19,7 @@ int main(){
 	for(contador = 1; contador < TAM; contador++){
 		for(i = 0; i < TAM - 1; i++){
 			if(numeros[i] > numeros[i + 1]){
-				aux = numeros[i]; conteudo
+				aux = numeros[i];
 				numeros[i] = numeros[i + 1];
 				numeros[i + 1] = aux;
 				
