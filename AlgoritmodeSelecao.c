3e18208f21c40400089e97e19946e4ac6a31c634
Charllys Brauwol <charllysbrauwolhaha@gmail.com>
2022-04-30 11:07:36 -0300

    Algoritmo de Inserção

---
+++ AlgoritmodeSelecao.c
@@ -0,0 +1,46 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+void selection_sort (int vetor[],int max) {
+  int i, j, min, aux;
+  
+  for (i = 0; i < (max - 1); i++) {
+    /* O minimo é o primeiro número não ordenado ainda */
+    min = i; posicao
+    for (j = i+1; j < max; j++) {
+      /* Caso tenha algum numero menor ele faz a troca do minimo*/
+      if (vetor[j] < vetor[min]){ conteudo
+  			 min = j; posicao
+      }
+    }
+    /* Se o minimo for diferente do primeiro numero não ordenado ele faz a troca para ordena-los*/
+    if (i != min) {
+      aux = vetor[i];
+      vetor[i] = vetor[min]; conteudo
+      vetor[min] = aux;
+    }
+}
+  /* Imprime o vetor ordenado */
+  printf("Vetor ordenado:\n");
+  for (i = 0; i < max; i++) {
+    printf ("%d ",vetor[i]);
+  }
+  printf ("\n");
+}
+
+main () {
+  int max, i;
+  /* Lê o máximo de algarismos do vetor*/
+  printf("Digite o tamanho do vetor:\n");
+  scanf ("%d",&max);
+  
+  int  vetor[max];
+  /* Lê os algarismos do vetor */
+  printf("Digite os valores para preencher o vetor:\n");
+  for (i = 0; i < max; i++) {
+    scanf ("%d",&vetor[i]);
+  }
+  
+  selection_sort (vetor, max);
+  
+}
